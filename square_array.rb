bf308b711f9277e6a34c76362fffe6db30e49f67
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-25 20:00:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,6 @@
 def square_array(array)
   # your code here
   newArray=[]
-  length = array.size 
-  counter = 0 
   
-  while counter < length 
-    newElement = array[counter] 
-    newElement = newElement * newElement
-    newArray.push(newElement)
-    counter = counter + 1 
-  end 
-  return newArray 
+  
 end
